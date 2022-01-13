d97e1f81145a5f9f21b54b53e20ec4b5014d7b61
Javier López Barba <javier@okteto.com>
2022-01-13 15:48:13 +0100

    Add personal flag
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+personal=""
+if [ -z $namespace ]; then
+  personal="--personal"
+fi
+
 if [ ! -z "$OKTETO_CA_CERT" ]; then
    echo "Custom certificate is provided"
    echo "$OKTETO_CA_CERT" > /usr/local/share/ca-certificates/okteto_ca_cert
@@ -8,7 +13,11 @@ if [ ! -z "$OKTETO_CA_CERT" ]; then
 fi
 
 namespace=$1
-echo running: okteto namespace "$namespace"
-okteto namespace "$namespace"
+echo running: okteto namespace "$namespace" $personal
+okteto namespace "$namespace" $personal
+
+echo running: okteto kubeconfig
+eval okteto kubeconfig
+
 k="/github/home/.kube/config"
 echo "::set-output name=kubeconfig::$k"
