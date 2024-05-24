b7824ae8a99d415b3defa4cad695e374627f01d6
Teresa Romero <teresa@okteto.com>
2024-05-24 15:45:37 +0200

    add log level
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,24 @@ if [ ! -z "$OKTETO_CA_CERT" ]; then
    update-ca-certificates
 fi
 
-echo running: okteto namespace "$namespace" $personal
-okteto namespace "$namespace" $personal
+log_level=$2
+if [ ! -z "$log_level" ]; then
+  if [ "$log_level" = "debug" ] || [ "$log_level" = "info" ] || [ "$log_level" = "warn" ] || [ "$log_level" = "error" ] ; then
+    log_level="--log-level ${log_level}"
+  else
+    echo "log-level supported: debug, info, warn, error"
+    exit 1
+  fi
+fi
+
+# https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging
+# https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+if [ "${RUNNER_DEBUG}" = "1" ]; then
+  log_level="--log-level debug"
+fi
+
+echo running: okteto namespace "$namespace" $personal $log_level
+okteto namespace "$namespace" $personal $log_level
 
 echo running: okteto kubeconfig
 eval okteto kubeconfig
